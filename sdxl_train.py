24ac3808fd86fdbde7f73b6bcf09639939522753
Isotr0py <2037008807@qq.com>
2024-01-21 22:25:52 +0800

    test

--- sdxl_train.py
+++ sdxl_train.py
@@ -714,7 +714,7 @@ def train(args):
 
     if is_main_process:
         src_path = src_stable_diffusion_ckpt if save_stable_diffusion_format else src_diffusers_model_path
-        print(text_encoder2.state_dict().keys())
+        print(type(unet),type(text_encoder1),type(text_encoder2))
         sdxl_train_util.save_sd_model_on_train_end(
             args,
             src_path,
