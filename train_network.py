e57473b90379e5821875f5fc1d197649458bdce4
Isotr0py <2037008807@qq.com>
2024-01-23 13:10:38 +0800

    test

--- train_network.py
+++ train_network.py
@@ -813,7 +813,7 @@ class NetworkTrainer:
                         if args.max_grad_norm != 0.0:
                             params_to_clip = accelerator.unwrap_model(network).get_trainable_params()
                             accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
-                    grads = [k for k,v in network.named_parameters() if v.grad is not None]
+                    grads = [v.grad for k,v in network.named_parameters() if v.grad is not None]
                     print(f"\nRank {accelerator.process_index}, loss: {loss:.5f}, grad: {grads[0].mean().tiem():.5f}, sync:{accelerator.sync_gradients}")
 
                     optimizer.step()
