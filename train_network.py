0d240b0dfa3effdb0f79fc258df4c7670c3a7fee
Isotr0py <2037008807@qq.com>
2024-01-23 13:20:17 +0800

    test

--- train_network.py
+++ train_network.py
@@ -814,7 +814,7 @@ class NetworkTrainer:
                             params_to_clip = accelerator.unwrap_model(network).get_trainable_params()
                             accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
                     w = [v for k,v in network.named_parameters() if v.grad is not None]
-                    print(f"\nRank {accelerator.process_index}, loss: {loss:.3f}, weight: {w[0].mean().item()}, grad: {w[0].grad.mean().item()}, sync:{accelerator.sync_gradients}")
+                    print(f"\nRank {accelerator.process_index}, weight: {w[0].mean().item()}, grad: {w[0].grad.mean().item()}, sync:{accelerator.sync_gradients}")
 
                     optimizer.step()
                     lr_scheduler.step()
